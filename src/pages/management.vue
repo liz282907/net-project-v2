b16a01a457296c323916426205dc208c77f21b69
liz282907 <422364804@qq.com>
2016-10-10 19:53:27 +0800

    add management

--- src/pages/management.vue
+++ src/pages/management.vue
@@ -1,20 +1,42 @@
 <template>
    <div class="manage-container">
-       <div class="form-part">
-            <el-breadcrumb separator="/" class="bread-crumb">
+       <div class="filter-part">
+            <el-breadcrumb separator="/" class="filter-breadcrumb">
                 <el-breadcrumb-item>全部</el-breadcrumb-item>
                 <el-breadcrumb-item>{{curTopic}}
                     <i class="iconfont">&#xe62a;</i>
                 </el-breadcrumb-item>
                 <el-breadcrumb-item>
                     <el-input placeholder="请输入内容" v-model="searchInput" style="width: 300px;">
-                          <el-select v-model="select" slot="prepend">
+                          <el-select v-model="searchSelect" slot="prepend">
                             <el-option label="关键词" value="keyword"></el-option>
                             <el-option label="分类" value="category"></el-option>
                           </el-select>
                         </el-input>
                 </el-breadcrumb-item>
             </el-breadcrumb>
+            <div class="filter-tags">
+                <section class="tag-bar clearfix">
+                    <span class="tag-key">分类：</span>
+                    <div class="tag-value">
+                        <ul class="tag-expand clearfix" >
+                            <li v-for="item in categories" class="tag">{{item}}</li>
+                        </ul>
+                        <span class="tag-more">展开<i class="iconfont arrow-closed">&#xe62a;</i></span>
+                    </div>
+
+                </section>
+                <section class="tag-bar clearfix">
+                    <span class="tag-key">显示内容：</span>
+                    <div class="tag-value">
+                        <ul class="tag-expand clearfix" >
+                            <li v-for="item in auditStates" class="tag">{{item}}</li>
+                        </ul>
+                        <span class="tag-more">展开<i class="iconfont arrow-closed">&#xe62a;</i></span>
+                    </div>
+
+                </section>
+            </div>
        </div>
 
    </div>
@@ -23,14 +45,17 @@
 
 <script>
 
-
 export default {
   components: {
     // "": ElBreadcrumb
   },
   data(){
     return {
-        curTopic: "习近平"
+        curTopic: "习近平",
+        searchInput: '',
+        searchSelect: '',
+        categories: [ "全部","出访","下基层","讲话","国际会议" ],
+        auditStates: [ "全部","未审核","已通过","未通过" ]
     }
   }
 };
